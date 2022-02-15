50eef5e84cb53fb0ecc3008163b5b83c9422ea66
Cody Hicks <hickscj5@gmail.com>
2022-02-15 16:56:54 -0700

    added the database for schema

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -1,5 +1,32 @@
 -- DROP DATABASE
 DROP DATABASE IF EXISTS ecommerce_db;
+CREATE DATABASE ecommerce_db
+USE ecommerce_db
+CREATE TABLE category (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
+  product_name 'NOT NULL'
+);
+CREATE TABLE product (
+   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL,
+   product_name  'NOT NULL'
+   price DECIMAL() NOT NULL,
+   stock INT UNSIGNED NOT NULL default 10,
+   category_id INT references category(id),
+);
+CREATE TABLE tag (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL,
+  tag_name ''
+
+);
+CREATE TABLE product_tag (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL,
+  product_id INT references product(id)
+  tag_id INT references tag(id)
+);
 
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
